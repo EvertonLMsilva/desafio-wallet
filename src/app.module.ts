bfe0ff8778db412ba50edeef97335076cc09ccbc
EvertonLMsilva <everton.l.m.silva@gmail>
2023-03-20 12:16:06 -0300

    feat(init): configure bull redis
    Related to DW-01

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
@@ -6,7 +7,13 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       envFilePath: '.env.develop',
       isGlobal: true
-    })
+    }),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT)
+      },
+    }),
   ],
   controllers: [],
   providers: [],
